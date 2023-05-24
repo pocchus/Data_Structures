6cde833126865f0b9f5b6f712b92c1b36d9f7a40
pocchus <123469715+pocchus@users.noreply.github.com>
2023-05-25 00:28:19 +0900

    update

---
+++ Deque/Deque_Linked.h
@@ -0,0 +1,16 @@
+#ifndef _DEQUE_LINKED_
+#define _DEQUE_LINKED_
+
+template <typename T>
+class Deque{
+private;
+    struct Node{
+        T data;
+        Node* prev;
+        Node* next;
+    };
+
+};
+
+
+#endif
