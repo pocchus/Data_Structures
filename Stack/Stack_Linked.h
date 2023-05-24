55f0e6e8100ad0de8fc12ba911f5cd5a44df460b
pocchus <pocchus@gmail.com>
2023-05-24 12:44:53 +0900

    exception handle
    added underflow exception
    changed file name

---
+++ Stack/Stack_Linked.h
@@ -0,0 +1,52 @@
+#ifndef _STACK_LINKED_
+#define _STACK_LINKED_
+
+#include <stdexcept>
+
+template <typename T>
+class Stack {
+private:
+	struct Node {
+		T data;
+		Node* next;
+	};
+	Node* _top;
+
+public:
+	Stack() :_top(nullptr){}
+
+	~Stack() {
+		while(!empty()){
+            pop();
+        }
+	}
+
+	bool empty() {
+		return _top == nullptr;
+	}
+
+	void push(T data) {
+		Node* newNode = new Node;
+		newNode->data = data;
+		newNode->next = _top;
+		_top = newNode;
+	}
+
+	void pop() {
+        if(empty()){
+            throw std::underflow_error("Stack underflow error. Stack is empty.");
+        }
+		Node* tmp = _top;
+		_top = _top->next;
+		delete tmp;
+	}
+
+	T top() {
+        if(empty()){
+            throw std::underflow_error("Stack underflow error. Stack is empty.");
+        }
+		return _top->data;
+	}
+};
+
+#endif
